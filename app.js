96a20dcc0a6d794fccd13b598c5530367e3b3b9c
Ngoding Bang <ngodingbang@gmail.com>
2023-12-07 20:54:18 +0700

    chore: create error handler function

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+import {
+  errorHandler,
+  notFoundErrorHandler,
+} from "./app/exceptions/handler.js";
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import express from "express";
-import createError from "http-errors";
 import logger from "morgan";
 
 const app = express();
 
-// view engine setup
+/**
+ * View engine setup.
+ */
 app.set("views", "views");
 app.set("view engine", "jade");
 
@@ -18,30 +23,7 @@ app.use(express.static("public"));
 
 app.use(router);
 
-app.use(
-  /**
-   * Catch 404 and forward to error handler.
-   */
-  (req, res, next) => next(createError(404)),
-);
-
-app.use(
-  /**
-   * Error handler.
-   *
-   * @param {Error} err
-   * @param {import("express").Request} req
-   * @param {import("express").Response} res
-   * @param {import("express").NextFunction} next
-   */
-  // eslint-disable-next-line no-unused-vars
-  (err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    res.status(err.status || 500);
-    res.render("error");
-  },
-);
+app.use(notFoundErrorHandler);
+app.use(errorHandler);
 
 export default app;
