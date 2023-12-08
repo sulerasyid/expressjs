ab287d824f4bddcd74fb3f5ac94c9ba42c057713
Ngoding Bang <ngodingbang@gmail.com>
2023-12-09 01:04:07 +0700

    fix: get app version using process.env

--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,11 @@
-import { createRequire } from "node:module";
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", (req, res) => res.json({ message: "Express" }));
 router.get("/app-version", (req, res) => {
-  const require = createRequire(import.meta.url);
-  const packageJson = require("../package.json");
-
   return res.json({
-    ["app-version"]: packageJson.version,
+    ["app-version"]: process.env.npm_package_version,
   });
 });
 
