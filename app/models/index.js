7d381fc3a3a32d0abd5c210b89fca49a94645e3c
Ngoding Bang <ngodingbang@gmail.com>
2023-12-09 00:42:46 +0700

    fix: change the way to instantiate prisma orm model

--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@prisma/client";
 import chalk from "chalk";
 
-function createModel() {
-  const model = new PrismaClient({
-    log: [{ emit: "event", level: "query" }],
-  });
+const model = new prisma.PrismaClient({
+  log: [{ emit: "event", level: "query" }],
+});
 
-  model.$on("query", event => {
-    console.info(`${chalk.blue("prisma:query")} ${event.query}`);
-    console.info(`${chalk.blue("prisma:params")}: ${event.params}`);
-    console.info(`${chalk.blue("prisma:duration")}: ${event.duration}ms`);
-    console.info(`${chalk.red("-----")}`);
-  });
+model.$on("query", event => {
+  console.info(`${chalk.blue("prisma:query")} ${event.query}`);
+  console.info(`${chalk.blue("prisma:params")}: ${event.params}`);
+  console.info(`${chalk.blue("prisma:duration")}: ${event.duration}ms`);
+  console.info(`${chalk.red("-----")}`);
+});
 
-  return model;
-}
-
-export const model = createModel();
-export * from "@prisma/client";
+export { model, prisma };
